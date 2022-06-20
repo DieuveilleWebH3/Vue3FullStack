5caa2c244fec3bc3c6fc6de8719f6bab977a5a70
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-06-20 15:28:41 +0200

    OK, improving signin page design, ..

--- vue_fullstack/src/components/SignIn.vue
+++ vue_fullstack/src/components/SignIn.vue
@@ -27,6 +27,80 @@ const handleSignIn = async () => {
 
 <template>
 
+    <div class="authincation h-100">
+        <div class="container h-100">
+
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+
+                    <div class="authincation-content">
+
+                        <div class="row no-gutters">
+
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+                                
+                                    <h2 class="text-center mb-4">Welcome !</h2>
+                                    <h4 class="text-center text-muted">Login to continue</h4>
+
+                                
+                                    <form method="post" class="needs-validation" novalidate>
+                                        
+                                        <div class="form-group">
+                                            <label for="email" class="mb-1"><strong>Email address *</strong></label>
+
+                                            <input type="email" style="color: black;" class="form-control" id="email" name="email" placeholder="Provide your email address" required>
+
+                                            <div class="invalid-feedback">
+                                                This field is mandatory.
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            <label for="password" class="mb-1"><strong>PASSWORD *</strong></label>
+                                            <input type="password" style="color: black;" class="form-control" id="password" name="password" placeholder="Saisissez votre mot de passe" required>
+
+                                            <div class="invalid-feedback">
+                                                this field is mandatory.
+                                            </div>
+                                        </div>
+
+                                        <div class="form-row d-flex justify-content-between mt-4 mb-2">
+                                            <div class="form-group">
+                                            <div class="custom-control custom-checkbox ml-1">
+                                                    <input type="checkbox" style="color: black;" class="custom-control-input" id="basic_checkbox_1">
+                                                    <label class="custom-control-label" for="basic_checkbox_1">Remember me</label>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <a class="" href="#">Forgot password ?</a>
+                                            </div>
+                                        </div>
+
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary text-white btn-block">Login</button>
+                                        </div>
+
+                                    </form>
+
+                                    <div class="new-account mt-3">
+                                        <p class="">You don't have an account yet ? <a class="text" style="color: #e4032e;" href="{% url 'register' %}"> Sign Up </a></p>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
     <h1> Sign In </h1>
 
     <form class="my-3" @submit.prevent="handleSignIn">
