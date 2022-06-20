69d27383dafc91cb9a65b135c472e567cae3e7a3
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-06-20 15:00:36 +0200

    OK, edited app vue to import bootstrap, ..

--- vue_fullstack/src/App.vue
+++ vue_fullstack/src/App.vue
@@ -1,12 +1,19 @@
 <script setup></script>
 
 <template>
+
   <div class="container">
     <header>
       <Navbar />
     </header>
     <router-view />
   </div>
+
 </template>
 
-<style></style>
+
+<style>
+
+  @import "bootstrap/dist/css/bootstrap.min.css";
+
+</style>
