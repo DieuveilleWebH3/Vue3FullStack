ae61ac2aa3da61a513437a6600c2fff73533c8f9
DieuveilleWebH3 <d.boussaellenga@h3hitema.fr>
2022-06-20 16:47:30 +0200

    OK, editing NavBar component, improving design, ..

--- vue_fullstack/src/components/NavBar.vue
+++ vue_fullstack/src/components/NavBar.vue
@@ -1,17 +1,58 @@
-<script setup>
+<script>
 
     import { RouterLink } from "vue-router";
     import router from "../router";
 
     const navigateToSignIn = () => {
-    router.push({ name: "signin" });
+        router.push({ name: "signin" });
     };
+
+
+    import {
+    MDBNavbar,
+    MDBNavbarToggler,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBCollapse,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem
+  } 
+  
+  from 'mdb-vue-ui-kit';
+  import { ref } from 'vue';
+  export default {
+    components: {
+      MDBNavbar,
+      MDBNavbarToggler,
+      MDBNavbarBrand,
+      MDBNavbarNav,
+      MDBNavbarItem,
+      MDBCollapse,
+      MDBDropdown,
+      MDBDropdownToggle,
+      MDBDropdownMenu,
+      MDBDropdownItem
+    },
+
+    setup() {
+      const collapse6 = ref(false);
+      const dropdown9 = ref(false);
+      return {
+        collapse6,
+        dropdown9
+      }
+    }
+  };
 
 </script>
 
 
 <template>
 
+    <!--
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
         <router-link to="/" class="navbar-brand">Home</router-link>
@@ -35,6 +76,67 @@
         </div>
 
     </nav>
+    -->
+
+    
+    
+  <MDBNavbar expand="lg" light bg="light" container>
+    <!-- Toggle button -->
+    <MDBNavbarToggler
+      target="#navbarCenterAlignExample"
+      @click="collapse6 = !collapse6"
+    ></MDBNavbarToggler>
+
+    <!-- Collapsible wrapper -->
+    <MDBCollapse id="navbarCenterAlignExample" v-model="collapse6">
+      <MDBNavbarNav center class="mb-2 mb-lg-0">
+        <!-- Center links -->
+        <MDBNavbarItem to="/" active>
+          Home
+        </MDBNavbarItem>
+
+        <MDBNavbarItem to="/about" href="#" active>
+          About
+        </MDBNavbarItem>
+
+        
+        <!-- Navbar dropdown -->
+        <MDBDropdown class="nav-item" v-model="dropdown9">
+          <MDBDropdownToggle
+            tag="a"
+            class="nav-link"
+            @click="dropdown9 = !dropdown9"
+            >Connexion</MDBDropdownToggle
+          >
+
+        <!-- The logic here is : when no user is connected, visitors see dropdown for login and register -->
+        <MDBDropdownMenu>
+            <MDBDropdownItem to="/signin" href="#"> Login </MDBDropdownItem>
+            <MDBDropdownItem to="/signup" href="#"> Register </MDBDropdownItem>
+        </MDBDropdownMenu>
+
+        <!-- The logic here is : when a user is connected, they see dropdown for logout and profile -->
+        <MDBDropdownMenu>
+            <MDBDropdownItem to="/profile" href="#"> Profile </MDBDropdownItem>
+            <MDBDropdownItem to="/logout" href="#"> Logout </MDBDropdownItem>
+        </MDBDropdownMenu>
+
+        </MDBDropdown>
+
+        <MDBNavbarItem
+          to="#"
+          disabled
+          tabindex="-1"
+          aria-disabled="true"
+        >
+          Disabled
+        </MDBNavbarItem>
+        <!-- Center links -->
+      </MDBNavbarNav>
+    </MDBCollapse>
+    <!-- Collapsible wrapper -->
+  </MDBNavbar>
+
 </template>
 
 
